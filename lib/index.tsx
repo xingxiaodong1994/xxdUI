ca68fc3285db1d115d2daa819372d245787d87c9
xingxiaodong <1204642219@qq.com>
2019-07-17 10:18:41 +0800

    可以编译ts文件，并打包了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,8 @@
-function greeter(person) {
+
+function greeter(person :string) {
     return "Hello, " + person;
 }
 
-let user = "Jane User";
+let user = "friend";
 
 document.body.innerHTML = greeter(user);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
+  mode:"production",
   entry:{
     index: "./lib/index.tsx",
   },
   output: {
     filename: "index.js",
     path:  path.resolve(__dirname, 'dist/lib')
-  }
+  },
+  module:{
+    rules:[
+      {
+        test: /\.tsx?$/,
+        loader:'awesome-typescript-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
