250cfa5da2e81cdd2561f596960d0980c8d87faf
xingxiaodong <1204642219@qq.com>
2019-08-07 17:01:53 +0800

    添加测试覆盖率命令 yarn ci

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,15 @@
-// import Button from './button';
-// import Xxd from './xxd';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
+export {default as Icon} from './icon/icon';
 
-ReactDOM.render(
-    <div>
-        <Icon name="file" onClick={(e)=>{console.log(e.target)}} />
-        <Icon name="chart" />
-        <Icon name="file" />
-    </div>,
-    document.getElementById('root')
-);
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Icon from './icon/icon';
+//
+// ReactDOM.render(
+//     <div>
+//         <Icon name="file" onClick={(e)=>{console.log(e.target)}} />
+//         <Icon name="chart" />
+//         <Icon name="file" />
+//     </div>,
+//     document.getElementById('root')
+// );
 
